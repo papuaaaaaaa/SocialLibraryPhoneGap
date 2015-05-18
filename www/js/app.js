17f3cf570df2a117a7fa30531d9afbed28a8e9ae
papuaaaaaaa <nakao_shinichirou@aratana.jp>
2015-05-19 00:52:28 +0900

    add storage service factory

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,20 @@
-var module = ons.bootstrap('my-app', ['onsen'])
-    .controller('AppController', function ($scope) {
-    })
-    .controller('PageController', function ($scope) {
-        ons.ready(function () {
-            // Init code here
-        });
-    });
+var module = ons.bootstrap('my-app', ['onsen', 'config'])
+    .factory('storage', ['$http', 'APP_CONF', function ($http, APP_CONF) {
+        return {
+            registerUser: function () {
+                //do something
+            }
+        };
+    }])
+    .controller('AppController', ['$scope', function ($scope) {
+    }])
+    .controller('PageController', ['$scope', function ($scope) {
+    }])
+    .controller('LoginController', ['$scope', 'storage', function ($scope, storage) {
+        $scope.signUp = function () {
+            storage.registerUser();
+        };
+    }])
+    .run(['$window', 'APP_CONF', function ($window, APP_CONF) {
+        Kii.initializeWithSite(APP_CONF.id, APP_CONF.key, KiiSite.JP);
+    }]);
