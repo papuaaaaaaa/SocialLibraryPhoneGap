e819eee010de6203373380487a00199445ee0153
papuaaaaaaa <nakao_shinichirou@aratana.jp>
2015-05-20 00:35:44 +0900

    add login method into strage service

--- www/js/app.js
+++ www/js/app.js
@@ -2,27 +2,41 @@ var module = ons.bootstrap('my-app', ['onsen', 'config'])
     .factory('storage', ['$http', 'APP_CONF', function ($http, APP_CONF) {
         return {
             registerUser: function (email, password) {
-                console.log(email + "aaa" + password);
                 // Create the KiiUser object
                 var user = KiiUser.userWithEmailAddress(email, password);
 
                 // Register the user, defining callbacks for when the process completes
                 user.register({
                     // Called on successful registration
-                    success: function(theUser) {
+                    success: function (theUser) {
                         // Print some info to the log
                         console.log("User registered!");
                         console.log(theUser);
                     },
                     // Called on a failed registration
-                    failure: function(theUser, errorString) {
+                    failure: function (theUser, errorString) {
                         // Print some info to the log
                         console.log("Error registering: " + errorString);
                     }
                 });
+            },
+            login: function (email, password) {
+                // Authenticate the user
+                KiiUser.authenticate(email, password, {
+                    // Called on successful registration
+                    success: function (theUser) {
+                        // Print some info to the log
+                        console.log("User authenticated!");
+                        console.log(theUser);
+                    },
+                    // Called on a failed authentication
+                    failure: function (theUser, errorString) {
+                        // Print some info to the log
+                        console.log("Error authenticating: " + errorString);
+                    }
+                });
             }
-
-        };
+        }
     }])
     .controller('AppController', ['$scope', function ($scope) {
     }])
@@ -33,10 +47,12 @@ var module = ons.bootstrap('my-app', ['onsen', 'config'])
         $scope.pass = ""
 
         $scope.signUp = function () {
-            if ($scope.mail != "" && $scope.pass != "") {
-                storage.registerUser($scope.mail, $scope.pass);
-            }
+            storage.registerUser($scope.mail, $scope.pass);
         };
+
+        $scope.login = function () {
+            storage.login($scope.mail, $scope.pass);
+        }
     }])
     .run(['$window', 'APP_CONF', function ($window, APP_CONF) {
         Kii.initializeWithSite(APP_CONF.id, APP_CONF.key, KiiSite.JP);
