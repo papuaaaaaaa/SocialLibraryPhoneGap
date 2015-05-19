42661c5aeece4dd3a4852e004b39d9582e72646c
papuaaaaaaa <nakao_shinichirou@aratana.jp>
2015-05-19 09:14:43 +0900

    create user flow sample

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,27 @@
 var module = ons.bootstrap('my-app', ['onsen', 'config'])
     .factory('storage', ['$http', 'APP_CONF', function ($http, APP_CONF) {
         return {
-            registerUser: function () {
-                //do something
+            registerUser: function (email, password) {
+                console.log(email + "aaa" + password);
+                // Create the KiiUser object
+                var user = KiiUser.userWithEmailAddress(email, password);
+
+                // Register the user, defining callbacks for when the process completes
+                user.register({
+                    // Called on successful registration
+                    success: function(theUser) {
+                        // Print some info to the log
+                        console.log("User registered!");
+                        console.log(theUser);
+                    },
+                    // Called on a failed registration
+                    failure: function(theUser, errorString) {
+                        // Print some info to the log
+                        console.log("Error registering: " + errorString);
+                    }
+                });
             }
+
         };
     }])
     .controller('AppController', ['$scope', function ($scope) {
@@ -11,8 +29,13 @@ var module = ons.bootstrap('my-app', ['onsen', 'config'])
     .controller('PageController', ['$scope', function ($scope) {
     }])
     .controller('LoginController', ['$scope', 'storage', function ($scope, storage) {
+        $scope.mail = ""
+        $scope.pass = ""
+
         $scope.signUp = function () {
-            storage.registerUser();
+            if ($scope.mail != "" && $scope.pass != "") {
+                storage.registerUser($scope.mail, $scope.pass);
+            }
         };
     }])
     .run(['$window', 'APP_CONF', function ($window, APP_CONF) {
